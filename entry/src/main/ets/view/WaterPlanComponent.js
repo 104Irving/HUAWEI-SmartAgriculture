df65d11cc3fbd25825765c91c7d40646087e6ac5
shibei <2939633973@qq.com>
2024-03-20 20:57:26 +0800

    版本0.0.0.2 上水课没写完版,优化了主页元素的呈现逻辑(未完成)

--- entry/src/main/ets/view/WaterPlanComponent.js
+++
@@ -1,9 +0,0 @@
-export struct WaterPlanComponent extends   {
-    constructor() { }
-    build() {
-            .width('100%')
-            .height('100%')
-            .backgroundColor('#f1f2f3');
-    }
-}
-//# sourceMappingURL=WaterPlanComponent.js.map
