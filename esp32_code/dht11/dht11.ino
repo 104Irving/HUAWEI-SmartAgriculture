cc2abe7f3df753467f1a2593e00f34d8c13d8b7f
matrix <2096838633@qq.com>
2024-03-21 14:49:56 +0800

    优化注释

--- esp32_code/dht11/dht11.ino
+++ esp32_code/dht11/dht11.ino
@@ -1,27 +1,26 @@
 #include <Arduino.h>
-#include <DHT.h>
+#include <DHT.h> //dht库
  
 #define DHTTYPE DHT11   // DHT 11
-#define DHTPIN 4 
+#define DHTPIN 4 //定义dht信号引脚
 DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
+  Serial.begin(115200); //设置串口波特率
+  dht.begin(); //启动dht
 }
  
 void loop() {
   float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  // Read temperature as Celsius (the default)
-  float f = dht.readTemperature(true);
-  // Check if any reads failed and exit early (to try again).
+  float t = dht.readTemperature(); //读取摄氏度
+  float f = dht.readTemperature(true); //读取华氏度
+  
 
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     delay(1000);
     return;
   }
-  // Check if any reads failed and exit early (to try again).
+  // 检查是否有数据读取失败，并重新尝试，间隔1s
 
   Serial.print("湿度:");
   Serial.print(h);
@@ -36,5 +35,6 @@ void loop() {
   Serial.println("℉");
 
   delay(10000);
+  //上报间隔10s
 }
 
