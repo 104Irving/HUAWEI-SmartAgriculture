5e4e7e777b2fae92988b5f987aff1afec4434e99
matrix <2096838633@qq.com>
2024-03-26 23:37:19 +0800

    更新电池电压算法

--- esp32_code/battery_monitor/battery_monitor.ino
+++ esp32_code/battery_monitor/battery_monitor.ino
@@ -1,7 +1,7 @@
 float volr = 0;  //读取的电压原始值
 float vol = 0;  //计算的电压值
 int battery = 0;
-int analogPin = 35;  //1号ADC引脚
+int analogPin = 34;  //1号ADC引脚
 
 void setup(){
 
@@ -10,17 +10,19 @@ void setup(){
 }
 
 void loop(){
-  volr = analogRead(analogPin);
-  vol = volr*5*3.3/4096;  //计算电压；
+  float x = analogRead(analogPin);
+  //volr = map(volr, 0, 880, 0 ,4095);
+  vol = -3.3126889904968743e-18*x*x*x*x*x*x*x+1.623217605075953e-14*x*x*x*x*x*x-3.3719331192434486e-11*x*x*x*x*x+3.847999653156838e-8*x*x*x*x-0.00002604499123955503*x*x*x+0.010452452154782833*x*x-2.3016342267679173*x+214.85012707853883;
+  vol = vol*5;  //计算电压；
   battery = 100*(vol - 2.5)/1.7;  //计算电量
   
   Serial.print("电压：");
   Serial.print(vol);
   Serial.println("v");
-  
-  Serial.print("电量");
+  Serial.println(volr);
+  Serial.print("电量：");
   Serial.print(battery);
   Serial.println("％");
-  delay(1000);
+  delay(500);
 
 }
