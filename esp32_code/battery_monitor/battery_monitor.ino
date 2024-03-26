5df1a7d31850a273919413746d300269e86443ef
matrix <2096838633@qq.com>
2024-03-27 00:20:25 +0800

    优化电池监测代码

--- esp32_code/battery_monitor/battery_monitor.ino
+++ esp32_code/battery_monitor/battery_monitor.ino
@@ -1,4 +1,3 @@
-float volr = 0;  //读取的电压原始值
 float vol = 0;  //计算的电压值
 int battery = 0;
 int analogPin = 34;  //1号ADC引脚
@@ -10,19 +9,20 @@ void setup(){
 }
 
 void loop(){
-  float x = analogRead(analogPin);
-  //volr = map(volr, 0, 880, 0 ,4095);
+  float x = analogRead(analogPin);  //读取电压ADC原始值
+  
   vol = -3.3126889904968743e-18*x*x*x*x*x*x*x+1.623217605075953e-14*x*x*x*x*x*x-3.3719331192434486e-11*x*x*x*x*x+3.847999653156838e-8*x*x*x*x-0.00002604499123955503*x*x*x+0.010452452154782833*x*x-2.3016342267679173*x+214.85012707853883;
+  //拟合曲线校正ADC
   vol = vol*5;  //计算电压；
-  battery = 100*(vol - 2.5)/1.7;  //计算电量
+  battery = 100*(vol - 3)/1.2;  //计算电量
   
   Serial.print("电压：");
   Serial.print(vol);
   Serial.println("v");
-  Serial.println(volr);
+  
   Serial.print("电量：");
   Serial.print(battery);
   Serial.println("％");
-  delay(500);
+  delay(1000);
 
 }
