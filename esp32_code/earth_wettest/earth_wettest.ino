c8b761fcdae58becaf6a83f96e0757a40f1a15e2
scientist <m17703407642_1@qq.com>
2024-03-27 23:44:14 +0800

    优化土壤湿度传感器，完成降雨传感器

--- esp32_code/earth_wettest/earth_wettest.ino
+++ esp32_code/earth_wettest/earth_wettest.ino
@@ -1,64 +1,62 @@
-//LingShun Lab
- 
- 
- 
-#define Moisture 2 //定义AO 引脚 为 D2
- 
-#define DO 4        //定义DO 引脚 为 D4
- 
- int data1;
- int data2;
- int data3;
- float ave;
-float per;
+#include <Arduino.h>
+
+#define MoisturePin 2 // 使用D2引脚用于湿度传感器
+#define DO 4           // 定义DO 引脚为 D4
+#define NumReadings 10 // 用于滤波的读数数量
+
+int moistureReadings[NumReadings]; // 存储滤波读数
+int readIndex = 0;                  // 当前读数的索引
+float total = 0;                    // 读数总和
+float average = 0;                  // 读数平均值
+float moisturePercent = 0;          // 湿度百分比
+unsigned long lastPrintTime = 0;    // 上次打印信息的时间
+
 void setup() {
- 
-  pinMode(Moisture, INPUT);//定义A0为输入模式
- 
-  pinMode(DO, INPUT);
- 
+  pinMode(MoisturePin, INPUT);
   Serial.begin(9600);
- 
+  // 初始化所有读数为0:
+  for (int thisReading = 0; thisReading < NumReadings; thisReading++) {
+    moistureReadings[thisReading] = 0;
+  }
 }
- 
+
 void loop() {
- 
-data1=analogRead(Moisture);
-data2=analogRead(Moisture);
-data3=analogRead(Moisture);
- ave=(data1+data2+data3)/3;
-per=(1-((ave-1400)/800))*100;
-  //串口返回测量数据
- 
-  //Serial.print("Moisture=");
+  // 从滤波总和中减去最后一个读数:
+  total = total - moistureReadings[readIndex];
+  // 读取新的湿度值:
+  moistureReadings[readIndex] = analogRead(MoisturePin);
+  // 将这个读数添加到total中:
+  total = total + moistureReadings[readIndex];
+  // 推进到下一个读数位置:
+  readIndex = (readIndex + 1) % NumReadings;
 
-  Serial.print("原始值：");
- Serial.print(analogRead(Moisture));
-Serial.print("|均值：");
-  Serial.print((data1+data2+data3)/3);//读取AO的数值
+  // 计算平均值:
+  average = total / NumReadings;
+  // 将平均值映射到湿度百分比:
+  moisturePercent = (1 - ((average - 1400) / 900)) * 100;
 
-  Serial.print("|湿润程度：");
- Serial.print(per);
-Serial.print("%");
-if(per<20)
-{
-  Serial.println("当前土壤非常干旱，需要大量灌溉");
-}
-else if(per<50)
-{
-  Serial.println("当前土壤较为干旱，需要少量灌溉");
-}
-else if(per<70)
-{
-  Serial.println("当前土壤湿润，不需要量灌溉");
-}
-else 
-{
-  Serial.println("当前土壤过于湿润，需要减少水分");
-}
- // Serial.print("|DO=");
- // Serial.println(digitalRead(DO));//读取DO的数值
- 
-  delay(1000);
- 
+  // 每隔一段时间打印信息:
+  if (millis() - lastPrintTime >= 1000) {
+    lastPrintTime = millis();
+    
+    // 打印时间戳和传感器读数:
+    Serial.print("时间: ");
+    Serial.print(millis() / 1000);
+    Serial.print("s | 原始值: ");
+    Serial.print(average);
+    Serial.print(" | 湿润程度: ");
+    Serial.print(moisturePercent);
+    Serial.println("%");
+
+    // 根据湿度百分比提供建议:
+    if (moisturePercent < 20) {
+      Serial.println("警告: 当前土壤非常干旱，需要大量灌溉。");
+    } else if (moisturePercent < 50) {
+      Serial.println("注意: 当前土壤较为干旱，需要适量灌溉。");
+    } else if (moisturePercent < 70) {
+      Serial.println("当前土壤湿润，维持当前水分状态即可。");
+    } else {
+      Serial.println("提醒: 当前土壤过于湿润，应考虑减少灌溉。");
+    }
+  }
 }
