370c0c05702a2f76c73b22579a9a95dfdf43d36a
matrix <2096838633@qq.com>
2024-03-27 21:46:34 +0800

    上传新版水位计代码

--- esp32_code/water_level/water_level.ino
+++ esp32_code/water_level/water_level.ino
@@ -1,19 +1,16 @@
-/*
-  【Arduino】168种传感器模块系列实验（资料代码+仿真编程+图形编程）
-  程序七：获取水位传感器模拟信号数据
-*/
-#define A0 5  //A0yingjiao d5
-int val = 0; //定义一个变量 val ，初始化值为0
 
+#define A0 35  
+int val = 0; //定义一个变量 val ，初始化值为0
+float water_height;
 void setup() {
-  Serial.begin(9600); // 设置波特率为9600
+  Serial.begin(115200); // 设置波特率为115200
 }
 
 void loop() {
   val = analogRead(A0); // 获取A0模拟口的数据，并赋值给 val 变量
   Serial.print("val = "); // 串口输出 val 当前的数据
   Serial.print(val);
-
+   
   if(val < 200) { // 如果 val 小于200
     Serial.println(" | dry"); // 说明很干，没有水
   } else if(val < 450) { // 如果 小于 450
@@ -28,5 +25,7 @@ Serial.println(" | water level: ~ 0-1 cm");
   } else {
     Serial.println(" | water level: over 4 cm");
   }
+
+  
   delay(1000); // 等待1秒
 }
