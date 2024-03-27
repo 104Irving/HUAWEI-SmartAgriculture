eecf9becb809ab4fa3b94701796b121bd87d286c
matrix <2096838633@qq.com>
2024-03-27 13:03:41 +0800

    对满电状态读数进行补充，使其稳定输出

--- esp32_code/battery_monitor/battery_monitor.ino
+++ esp32_code/battery_monitor/battery_monitor.ino
@@ -11,7 +11,7 @@ float b = 0;
 float c = 0;
 */
 
-int Value = analogRead(analogPin);
+int Value = analogRead(analogPin)+8;
 
 void setup(){
 
@@ -24,7 +24,11 @@ void loop(){
   Value = Filter_Value;
   x = Filter_Value;
   //x = analogRead(analogPin);  //读取电压ADC原始值
-  
+
+  if((x-880)<5||(880-x)<5)
+  x = 880;
+  //满电补偿
+
   vol = -3.3126889904968743e-18*x*x*x*x*x*x*x+1.623217605075953e-14*x*x*x*x*x*x-3.3719331192434486e-11*x*x*x*x*x+3.847999653156838e-8*x*x*x*x-0.00002604499123955503*x*x*x+0.010452452154782833*x*x-2.3016342267679173*x+214.85012707853883;
   //拟合曲线校正ADC
   vol = vol*5;  //计算电压；
@@ -39,11 +43,11 @@ void loop(){
   vol = (a+b+c)/3;
   //减小波动
 */
-
+  
   Serial.print("电压：");
   Serial.print(vol);
   Serial.println("v");
-  //Serial.println(Filter_Value);
+  //Serial.println(x);
   Serial.print("电量：");
   Serial.print(battery);
   Serial.println("％");
@@ -52,13 +56,13 @@ void loop(){
 
 }
 
-#define FILTER_A 3  //阈值
+#define FILTER_A 2  //阈值
 int Filter(){
   int NewValue;
-  NewValue = analogRead(analogPin);
+  NewValue = analogRead(analogPin)+8;
   if(((NewValue-Value)>FILTER_A)||(Value-NewValue)>FILTER_A)
   return Value;
   else
   return NewValue; 
 }
-//对ADC原始值限幅滤波
+//对ADC原始值进行限幅滤波
