dfece39168116322a708e732b7ad74fd9a8d1db9
Administrator <3408619575@qq.com>
2024-06-03 20:30:28 +0800

    新东西

--- BACK/src/main/java/com/qdong/back/BackApplication.java
+++ BACK/src/main/java/com/qdong/back/BackApplication.java
@@ -6,6 +6,7 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.mybatis.spring.annotation.MapperScan;
 import org.opencv.highgui.HighGui;
+import org.opencv.videoio.VideoCapture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,8 +19,6 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static com.sun.org.apache.xerces.internal.util.DOMUtil.setVisible;
-
 @SpringBootApplication
 @MapperScan("com.qdong.back.mapper")
 public class BackApplication {
@@ -28,21 +27,5 @@ public class BackApplication {
 	private static Socket socket;
 	public static void main(String[] args) throws IOException {
         SpringApplication.run(BackApplication.class, args);
-		ServerSocket serverSocket = new ServerSocket(8081);
-		socket = serverSocket.accept();
-		InputStream inputStream = socket.getInputStream();
-		if(inputStream != null){
-			System.out.println("Client connected: " + socket);
-			canvasFrame = new CanvasFrame("Server Frame");
-			canvasFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			canvasFrame.setSize(640, 480);
-			canvasFrame.setVisible(true);
-			frameGrabber = FrameGrabber.createDefault(0);
-			frameGrabber.start();
-			Frame frame;
-			while ((frame = frameGrabber.grab()) != null) {
-				canvasFrame.showImage(frame);
-			}
-		}
 	}
 }
