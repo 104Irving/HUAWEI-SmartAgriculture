bbe32895b1007ec941781d48320950623dcd5f90
tyj <3408619575@qq.com>
2024-06-09 20:51:13 +0800

    新东西

---
+++ BACK/src/main/java/com/qdong/back/HttpVideoReceiver.java
@@ -0,0 +1,46 @@
+package com.qdong.back;
+
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import static org.bytedeco.opencv.global.opencv_core.flip;
+
+
+public class HttpVideoReceiver {
+
+    public static void main(String[] args) throws Exception {
+        // 创建一个视频捕捉对象
+        VideoCapture capture = new VideoCapture();
+        // 打开视频流
+        capture.open("http://192.168.16.4");
+
+        // 创建一个窗口来显示视频
+        CanvasFrame canvasFrame = new CanvasFrame("Video", CanvasFrame.getDefaultGamma() / capture.get(Videoio.CAP_PROP_GAMMA));
+        canvasFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Mat frame = new Mat();
+        // 读取视频流并显示
+        while (canvasFrame.isVisible() && capture.read(frame)) {
+            // 从捕捉对象中读取帧
+            capture.read(frame);
+
+            // 翻转帧以适应Java窗口
+            flip(frame, frame, 1);
+            int width = frame.cols();
+            int height = frame.rows();
+            byte[] data = new byte[width * height * (int)frame.elemSize()];
+            frame.data().get(data);
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+
+            image.getRaster().setDataElements(0, 0, width, height, data);
+            canvasFrame.showImage(image);
+        }
+
+        // 释放资源
+        capture.release();
+        canvasFrame.dispose();
+    }
+}
